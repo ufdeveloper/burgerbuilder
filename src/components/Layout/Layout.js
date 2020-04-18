cf8dbf52b113d2f99ce7da6980386b46b1b73cf0
Shantanu Sardal <ufdeveloper@gmail.com>
2020-04-18 18:10:18 -0400

    added Layout component

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+
+const layout = (props) => (
+    <Aux>
+        <div>
+            ToolBar, SideDrawer, BackDrop
+        </div>
+        <main>
+            {props.children}
+        </main>
+    </Aux>
+);
+
+export default layout;
