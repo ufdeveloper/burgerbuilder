292c2d5e4ac3a3700cb425ed92e45f77ac72c3d7
Shantanu Sardal <ufdeveloper@gmail.com>
2020-04-19 16:35:13 -0400

    initialize app with empty burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,19 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
 
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} ingredientType={igKey}/>
             });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please add some ingredients to your burger!</p>;
+    }
 
     return (
         <div className={classes.Burger}>
