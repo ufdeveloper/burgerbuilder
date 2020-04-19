7d2a8fce2227172e927b34352498683699f02702
Shantanu Sardal <ufdeveloper@gmail.com>
2020-04-19 16:22:13 -0400

    added ingredients via state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,18 @@ import classes from './Burger.css';
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
+
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} ingredientType={igKey}/>
+            });
+        });
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient ingredientType="bread-top" />
-            <BurgerIngredient ingredientType="cheese" />
-            <BurgerIngredient ingredientType="meat" />
+            {transformedIngredients}
             <BurgerIngredient ingredientType="bread-bottom" />
         </div>
     );
